b709a51f70449084591445049f5d9cd34a274ec8
federicascognamiglio <scognamigliofede99@gmail.com>
2025-03-20 14:16:28 +0100

    feat: footer completed

--- resources/views/Partials/footer.blade.php
+++ resources/views/Partials/footer.blade.php
@@ -1,1 +1,55 @@
-<h2>Footer</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-30 d-flex">
+            <div class="footer-text">
+                <h6>DC COMICS</h6>
+                <ul class="pb-20">
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+                <h6>SHOP</h6>
+                <ul>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <div class="footer-text">
+                <h6>DC</h6>
+                <ul>
+                    <li>Terms Of Use</li>
+                    <li>Privacy policy (New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-70">
+            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+        </div>
+    </div>
+</div>
+<div class="footer-banner">
+    <div class="container d-flex align-center justify-between">
+        <button class="outline-btn">SIGN UP NOW!</button>
+        <div class="d-flex align-center g-20">
+            <h6>FOLLOW US</h6>
+            <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook icon">
+            <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter icon">
+            <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="Youtube icon">
+            <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest icon">
+            <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope icon">
+        </div>
+    </div>
+</div>
